f3c5dd4958774f049d7a25f1e66ce3482f6d14c9
adasilvana <55792633+adasilvana@users.noreply.github.com>
2023-05-31 12:34:40 +0200

    Update fct_inventory.sql

--- parte-3/project/retail-data-warehouse/fct/fct_inventory.sql
+++ parte-3/project/retail-data-warehouse/fct/fct_inventory.sql
@@ -9,11 +9,11 @@ CREATE TABLE fct.inventory
                             , inicial SMALLINT
                             , final   SMALLINT
                             
-                            constraint fk_tienda_id_inventory
+                            , constraint fk_tienda_id_inventory
                             foreign key (tienda)
                             references dim.store_master (codigo_tienda)
                             
-                            constraint fk_sku_id_inventory
+                            , constraint fk_sku_id_inventory
                             foreign key (sku)
                             references dim.product_master (codigo_producto)
                  );
