90a9b4a1a4bb63253e2b7cfd795449c210e5cdaf
adasilvana <55792633+adasilvana@users.noreply.github.com>
2023-05-31 11:07:27 +0200

    Create cost.sql

---
+++ parte-3/project/retail-data-warehouse/stg/cost.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS stg.cost;
+    
+CREATE TABLE stg.cost
+                 (
+                              codigo_producto    VARCHAR(10)
+                            , costo_promedio_usd DECIMAL
+                 );
