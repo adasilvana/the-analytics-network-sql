c38fd0e18913c8f4bd49088e26d3679f9cd4fe92
adasilvana <55792633+adasilvana@users.noreply.github.com>
2023-05-31 11:55:46 +0200

    Create fct_order_line_sale.sql

---
+++ parte-3/project/retail-data-warehouse/fct/fct_order_line_sale.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS fct.order_line_sale;
+    
+CREATE TABLE fct.order_line_sale
+                 (
+                              orden_id   SERIAL PRIMARY KEY
+					 		, orden      VARCHAR(10) 
+                            , producto   VARCHAR(10)
+                            , tienda     SMALLINT
+                            , fecha      date
+                            , cantidad   int
+                            , venta      decimal(18,5)
+                            , descuento  decimal(18,5)
+                            , impuestos  decimal(18,5)
+                            , creditos   decimal(18,5)
+                            , moneda     varchar(3)
+                            , pos        SMALLINT
+                            , is_walkout BOOLEAN
+					 
+					 		constraint fk_tienda_id_order_line_sale
+                            foreign key (tienda)
+                            references dim.store_master (codigo_tienda)
+                            
+                            constraint fk_sku_id_order_line_sale
+                            foreign key (producto)
+                            references dim.product_master (codigo_producto)
+                 );
+				 
