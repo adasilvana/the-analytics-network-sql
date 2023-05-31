568cb78730c3d37717904aa4b8b456b16a74c554
adasilvana <55792633+adasilvana@users.noreply.github.com>
2023-05-31 11:10:29 +0200

    Create monthly_average_fx_rate.sql

---
+++ parte-3/project/retail-data-warehouse/stg/monthly_average_fx_rate.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS stg.monthly_average_fx_rate;
+    
+CREATE TABLE stg.monthly_average_fx_rate
+                 (
+                              mes                 DATE
+                            , cotizacion_usd_peso DECIMAL
+                            , cotizacion_usd_eur DECIMAL
+                            , cotizacion_usd_uru  DECIMAL
+                 );
