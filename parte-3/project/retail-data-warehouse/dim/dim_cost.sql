eec37b30949a0e91480dedafe424d5ac39f8f7d7
adasilvana <55792633+adasilvana@users.noreply.github.com>
2023-05-31 12:32:53 +0200

    Update dim_cost.sql

--- parte-3/project/retail-data-warehouse/dim/dim_cost.sql
+++ parte-3/project/retail-data-warehouse/dim/dim_cost.sql
@@ -6,7 +6,7 @@ CREATE TABLE dim.cost
                               codigo_producto    VARCHAR(10) PRIMARY KEY
                             , costo_promedio_usd DECIMAL
 					 		
-					 		              constraint fk_tienda_id_cost
+			    , constraint fk_tienda_id_cost
                             foreign key (codigo_producto)
                             references dim.product_master (codigo_producto)
                  );
