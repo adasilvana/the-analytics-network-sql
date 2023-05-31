b39f0b75adf0b3291b7c45785db67bcfb4e8c9f1
adasilvana <55792633+adasilvana@users.noreply.github.com>
2023-05-31 12:34:17 +0200

    Update dim_suppliers.sql

--- parte-3/project/retail-data-warehouse/dim/dim_suppliers.sql
+++ parte-3/project/retail-data-warehouse/dim/dim_suppliers.sql
@@ -2,10 +2,10 @@ DROP TABLE IF EXISTS dim.suppliers;
 
 CREATE TABLE dim.suppliers
 (
-	id_supplier SERIAL PRIMARY KEY
+    id_supplier SERIAL PRIMARY KEY,
     codigo_producto varchar(255),
     nombre varchar(255),
-    is_primary bool
+    is_primary bool,
 	
 	constraint fk_tienda_id_suppliers
 	foreign key (codigo_producto)
