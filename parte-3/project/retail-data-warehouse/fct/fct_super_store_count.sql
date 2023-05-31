2364c8d8322c9936b9f059505fa5591642d029bf
adasilvana <55792633+adasilvana@users.noreply.github.com>
2023-05-31 12:04:12 +0200

    Create fct_super_store_count.sql

---
+++ parte-3/project/retail-data-warehouse/fct/fct_super_store_count.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS fct.super_store_count;
+    
+CREATE TABLE fct.super_store_count
+                 (
+					 		  id_store_count SERIAL PRIMARY KEY
+                            , tienda SMALLINT
+                            , fecha  VARCHAR(10)
+                            , conteo SMALLINT
+					 
+					 		constraint fk_tienda_id_super_store_count
+                            foreign key (tienda)
+                            references dim.store_master (codigo_tienda)
+                 );
