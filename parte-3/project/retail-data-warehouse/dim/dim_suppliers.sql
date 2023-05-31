bc36fd33b2a32214d0e0dc6ca79de063e7d9e65e
adasilvana <55792633+adasilvana@users.noreply.github.com>
2023-05-31 12:18:29 +0200

    Create dim_suppliers.sql

---
+++ parte-3/project/retail-data-warehouse/dim/dim_suppliers.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS dim.suppliers;
+
+CREATE TABLE dim.suppliers
+(
+	id_supplier SERIAL PRIMARY KEY
+    codigo_producto varchar(255),
+    nombre varchar(255),
+    is_primary bool
+	
+	constraint fk_tienda_id_suppliers
+	foreign key (codigo_producto)
+	references dim.product_master (codigo_producto)
+);
